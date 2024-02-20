30e7f4dc099e8ebdbb0c17732c23bf6538a87ca7
VivekPanchal <ivivekpanchal123@gmail.com>
2024-02-20 20:48:32 +0530

    partial commit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect('mongodb+srv://myadmin:mypassword@smeapi.bzcafad.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
